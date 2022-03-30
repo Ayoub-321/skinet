e37ed3ada5377dc849872a31f7fc117ce537a87a
Ayoub-321 <ayoubaissou.info@gmail.com>
2022-03-30 10:52:07 +0100

    end of section3

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using Core.Interfaces;
+using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
@@ -7,10 +9,13 @@ string connString = builder.Configuration.GetConnectionString("DefaultConnection
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<Infrastructure.Data.StoreContext>(x =>
+builder.Services.AddDbContext<StoreContext>(x =>
 {
     x.UseSqlite(connString);
 });
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -22,6 +27,15 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+// migrate any database changes on startup (includes initial db creation)
+using (var scope = app.Services.CreateScope())
+{
+    var dataContext = scope.ServiceProvider.GetRequiredService<StoreContext>();
+    var loggerFactory = scope.ServiceProvider.GetRequiredService<ILoggerFactory>();
+    dataContext.Database.Migrate();
+    await StoreContextSeed.SeedAsync(dataContext, loggerFactory);
 }
 
 app.UseHttpsRedirection();
